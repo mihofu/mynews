990fe95a96834bf6d3a1c23a34cf2592c4583f7c
mihof <bmo_lumpy@yahoo.co.jp>
2019-11-13 13:22:23 +0000

    third commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -12,10 +13,6 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
     
     public function edit()
     {
@@ -25,6 +22,20 @@ class ProfileController extends Controller
     public function update()
     {
         return redirect('admin/profile/edit');
+    }
+    
+        public function create(Request $request)
+    {
+      $this->validate($request, Profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+      
+      unset($form['_token']);
+    
+      $profile->fill($form);
+      $profile->save();
+      
+        return redirect('admin/profile/create');
     }
     
 
