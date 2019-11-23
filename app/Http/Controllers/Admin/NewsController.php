c6cfffc7ae6e7b7a5ca01da9f72386fa0cf054d5
mihof <bmo_lumpy@yahoo.co.jp>
2019-11-23 00:02:47 +0000

    11/23-2

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\News;
+use Carbon\Carbon;
+use App\History;
 
 class NewsController extends Controller
 {
@@ -68,6 +70,11 @@ public function update(Request $request)
       }
       unset($news_form['_token']);
       $news->fill($news_form)->save();
+      
+      $history = new History;
+      $history->news_id = $news->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
 
       return redirect('admin/news');
   }
